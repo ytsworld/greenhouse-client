49fec89f131e28ebecc24ff8305c6cdd2971bad2
Stefan Weisskopf <stefanweisskopf@gmx.de>
2019-09-22 14:05:30 +0200

    wait longer before power saving mode

--- ansible/resources/greenhouse/home/pi/reducePowerConsumption.sh
+++ ansible/resources/greenhouse/home/pi/reducePowerConsumption.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-timeToPowerSave=60
+timeToPowerSave=300
 
-echo 1 > /sys/devices/platform/soc/3f980000.usb/buspower
 
 sleep $timeToPowerSave
 
-sudo systemctl stop ssh
+systemctl stop ssh
 /opt/vc/bin/tvservice -o
 echo 0 > /sys/devices/platform/soc/3f980000.usb/buspower
 echo 0 | sudo tee /sys/class/leds/led0/brightness 
