9e6c3deff08ccf0313d577eb495937f48aad396a
Stefan Weißkopf <stefanweisskopf@gmx.de>
2019-10-01 20:53:38 +0200

    skip switch off usb

--- ansible/resources/greenhouse/home/pi/reducePowerConsumption.sh
+++ ansible/resources/greenhouse/home/pi/reducePowerConsumption.sh
@@ -7,6 +7,8 @@ sleep $timeToPowerSave
 
 systemctl stop ssh
 /opt/vc/bin/tvservice -o
-echo 0 > /sys/devices/platform/soc/3f980000.usb/buspower
 echo 0 | sudo tee /sys/class/leds/led0/brightness 
 echo 0 | sudo tee /sys/class/leds/led1/brightness
+
+# TODO: Needs analysis - this seems to break the client
+# echo 0 > /sys/devices/platform/soc/3f980000.usb/buspower
