fb42b0fb88a17568fde0ba9333e41a9de5048469
Stefan Weißkopf <stefanweisskopf@gmx.de>
2019-10-04 23:08:25 +0200

    configurable server url (#1)
    * configurable server url
    * dedicated error indication led
    * update documentation

--- ansible/resources/greenhouse/home/pi/runGreenhouse.sh
+++ ansible/resources/greenhouse/home/pi/runGreenhouse.sh
@@ -1,3 +1,8 @@
 #! /bin/sh
 
-GOOGLE_APPLICATION_CREDENTIALS=/home/pi/greenhouse-client.sa.json /home/pi/greenhouse-client
+GOOGLE_APPLICATION_CREDENTIALS="/home/pi/greenhouse-client.sa.json"
+export GOOGLE_APPLICATION_CREDENTIALS
+GREENHOUSE_RECEIVER_URL="{{ greenhouse_recv_url }}"
+export GREENHOUSE_RECEIVER_URL
+
+/home/pi/greenhouse-client
